4137036e2c9da51c4abc1aac1c0fb712a8a6c19b
Kevin Pak <kedpak@Kevins-MacBook-Pro.local>
2026-04-26 22:59:28 -0700

    create base class

--- lsm_tree.py
+++ lsm_tree.py
@@ -12,4 +12,21 @@ class MemTable:
     def get(self, key):
         return self.data.get(key)
     
-    
+    def is_full(self):
+        return len(self.data) >= len(self.max_size)
+
+    def flush_to_sstable(self, path):
+        with open(path, "w") a f:
+            for k, v in self.data.item():
+                f.write(json.dumps({"k": k, "v": v}) + "\n")
+        self.data.clear()
+
+
+
+class LSMDM:
+    def __init__(self, data_dir):
+        self.data_dir = data_dir
+        os.makedirs(data_dir, exist_ok=True)
+        self.memtable = MemTable()
+        self.sstables = [] # newest last, search newest first
+        self.wal = open(os.path.join(data_dir, "wal.log"), "a") # write ahead log
